fe20d07e32a5d3f7b07af88d0dd7e9d68e51a232
Vladislav Perfiliev <vladis.perfiljev2011@yandex.ru>
2024-05-24 21:38:05 +0400

    done2

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -3,23 +3,23 @@ package ru.kata.spring.boot_security.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.dao.RoleDAO;
 import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 
 import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
-public class RoleServiceImpl implements RoleService {
-    private final RoleDAO roleRepository;
+public class RoleServiceImpl implements RoleService{
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public RoleServiceImpl(RoleDAO roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository) {
         this.roleRepository = roleRepository;
     }
 
     @Override
-    public List<Role> getListRoles() {
+    public List<Role> getAllRoles() {
         return roleRepository.findAll();
     }
 }
